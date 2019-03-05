12352948dba4efabfb1ac79bedaca1bfa8e0261f
AnsonYeung <s151204@tanghin.edu.hk>
2019-03-05 16:05:18 +0800

    Fixing Font Name

--- test/t1.pas
+++ test/t1.pas
@@ -10,7 +10,7 @@ c: DWord;
 
 Begin
 
-	SetConsoleFont('Lucida Consolas', 10, 20);
+	SetConsoleFont('Lucida Console', 10, 20);
 	SetConsoleSize(120, 30);
 	SetConsoleTitle('Console.ppu Test Progarm');
 	start := Time();
